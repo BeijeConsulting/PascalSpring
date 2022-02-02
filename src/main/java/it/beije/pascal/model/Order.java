76d186df2d4f45a0dddf2cbbcb91d9af6e6ca725
ema29 <ema29@DESKTOP-5H4N041>
2022-02-02 12:51:11 +0100

    risoluzione conflitti

--- src/main/java/it/beije/pascal/model/Order.java
+++ src/main/java/it/beije/pascal/model/Order.java
@@ -1,6 +1,8 @@
 package it.beije.pascal.model;
 
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,32 +11,79 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "order")
-public class Order {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private int id;
-	
-	@Column(name = "date")
-	LocalDate date;
-	
-	@Column(name = "amount")
-	double amount;
-	
-	@Column(name = "id")
-	int user_id;
-	
-}
 
-//CREATE TABLE `order` (
-//		  `id` int(11) NOT NULL AUTO_INCREMENT,
-//		  `creation_datetime` datetime NOT NULL,
-//		  `amount` double NOT NULL,
-//		  `user_id` int(11) NOT NULL,
-//		  PRIMARY KEY (`id`),
-//		  KEY `fk_user_idx` (`user_id`),
-//		  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-//		) ENGINE=InnoDB;
+
+@Entity
+@Table(name = "`order`")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+
+	@Column(name="user_id")
+	private Integer userId;
+	
+	@Column(name="amount")
+	private Double amount;
+	
+	@Column(name="creation_datetime")
+	private LocalDateTime dateTime;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	public String getDateTimeAsString() {
+		return dateTime.format(DateTimeFormatter.ISO_DATE_TIME);
+	}
+
+	public void setDateTime(String dateTime) {
+		this.dateTime = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_DATE_TIME);
+	}
+	
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder("{id: ").append(id)
+				.append(", userId: ").append(userId)
+				.append(", amount: ").append(amount)
+				.append(", dateTime: ").append(dateTime)
+				.append("}");
+		
+		return builder.toString();
+	}
+
+}
