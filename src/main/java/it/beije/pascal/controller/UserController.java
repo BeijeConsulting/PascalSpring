1c8cba2ed05387df8966eb178d10e3981164eed9
ema29 <ema29@DESKTOP-5H4N041>
2022-02-02 12:52:22 +0100

    controller for user

--- src/main/java/it/beije/pascal/controller/UserController.java
+++ src/main/java/it/beije/pascal/controller/UserController.java
@@ -1,7 +1,5 @@
 package it.beije.pascal.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import it.beije.pascal.model.User;
-import it.beije.pascal.repository.RubricaRepository;
 import it.beije.pascal.repository.UserRepository;
 
 @Controller
