61635ada5165c6a0a3e553aaf8a134fd360caf47
ivfly <iv0fly@gmail.com>
2022-02-02 11:53:04 +0100

    + @Query

--- src/main/java/it/beije/pascal/controller/RubricaController.java
+++ src/main/java/it/beije/pascal/controller/RubricaController.java
@@ -1,9 +1,5 @@
 package it.beije.pascal.controller;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.beije.pascal.model.Contatto;
-import it.beije.pascal.repository.RubricaRepository;
 import it.beije.pascal.service.RubricaService;
 
 
@@ -27,10 +22,7 @@ public class RubricaController {
 	
 	@Autowired
 	private RubricaService rubricaService;
-
-	@Autowired
-	private RubricaRepository rubricaRepository;
-
+	
 
 	@RequestMapping(value = "/rubrica", method = RequestMethod.GET)
 	public String lista(Model model, @RequestParam(value = "surname", required = false) String cognome) {
@@ -40,9 +32,8 @@ public class RubricaController {
 		//RubricaService.getList()
 		//RubricaService rubricaService = new RubricaService();
 		
-		//List<Contatto> contatti = rubricaService.getList();
+		List<Contatto> contatti = rubricaService.getList(cognome);
 
-		List<Contatto> contatti = cognome != null ? rubricaRepository.findByCognome(cognome) : rubricaRepository.findAll();
 		System.out.println("contatti : " + contatti.size());
 		
 		model.addAttribute("contatti", contatti);
