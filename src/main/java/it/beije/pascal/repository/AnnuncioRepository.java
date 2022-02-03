771f67720f9cbe6eea8d986e951c13732dba6686
Francesco Cimino <franc@LAPTOP-GQ5VE1T2>
2022-02-03 12:02:20 +0100

    -repo annuncio

--- src/main/java/it/beije/pascal/repository/AnnuncioRepository.java
+++
@@ -1,9 +0,0 @@
-package it.beije.pascal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.beije.pascal.model.Annuncio;
-
-public interface AnnuncioRepository extends JpaRepository<Annuncio, Integer> {
-
-}
