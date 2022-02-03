48ca5cee8da1898379eb4e91dc8a2d858fe14d10
ema29 <ema29@DESKTOP-5H4N041>
2022-02-03 12:23:51 +0100

    merge from main

--- src/main/java/it/beije/pascal/repository/AnnunciSalvatiRepository.java
+++ src/main/java/it/beije/pascal/repository/AnnunciSalvatiRepository.java
@@ -1,10 +1,11 @@
 package it.beije.pascal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import it.beije.pascal.model.AnnunciSalvati;
 
-
+@Repository
 public interface AnnunciSalvatiRepository extends JpaRepository<AnnunciSalvati, Integer>{
 
 }
