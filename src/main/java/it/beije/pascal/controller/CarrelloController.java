ab7f5bc12dcb854970befb8a060d9f1c084f221c
Andrea Gliori <andrea.gliori@gmail.com>
2022-02-08 10:09:24 +0100

    -

--- src/main/java/it/beije/pascal/controller/CarrelloController.java
+++ src/main/java/it/beije/pascal/controller/CarrelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import it.beije.pascal.model.Order_item;
+import it.beije.pascal.model.OrderItem;
 
 @Controller
 public class CarrelloController {
@@ -12,10 +12,10 @@ public class CarrelloController {
 	@GetMapping(value="/carrello")
 	public String carrello(@RequestParam int idProdotto, @RequestParam int quantity, @RequestParam double prezzo) {
 		
-		Order_item item = new Order_item();
-		item.setProduct_id(idProdotto);
+		OrderItem item = new OrderItem();
+		item.setProductId(idProdotto);
 		item.setQuantity(quantity);
-		item.setSell_price(prezzo);
+		item.setSellPrice(prezzo);
 //		item.setOrder_id();
 		return null;
 		
